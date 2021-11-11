55fdbcfe7a2d5e23360e3f9b3b5848d46d99e5ac
JoaoGabriel <joao.g.castro20@gmail.com>
2021-11-11 14:29:46 -0300

    Resultado de aprovação do aluno

---
+++ resultadoAluno/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace resultadoAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, media;
+            Console.WriteLine("Digite a primeira nota do aluno:");
+            nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota do aluno:");
+            nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a terceira nota do aluno:");
+            nota3 = double.Parse(Console.ReadLine());
+
+            media = (nota1 + nota2 + nota3) / 3;
+
+            if (media >= 7)
+            {
+                Console.WriteLine($"A média do aluno foi de {media.ToString("0.00")}.\n Aluno foi aprovado!");
+            } else if (media >= 4)
+            {
+                Console.WriteLine($"A média do aluno é de {media.ToString("0.00")}.\n Aluno está em prova final!");
+            } else
+            {
+                Console.WriteLine($"A média do aluno foi de {media.ToString("0.00")}.\n Aluno foi reprovado.");
+            }
+        }
+    }
+}
